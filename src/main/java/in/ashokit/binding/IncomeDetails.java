c2bf3dd844255632975944238ac61fcc7145b315
KamalTade <110621652+KamalTade@users.noreply.github.com>
2022-10-15 13:06:50 +0530

    Update IncomeDetails.java

--- src/main/java/in/ashokit/binding/IncomeDetails.java
+++ src/main/java/in/ashokit/binding/IncomeDetails.java
@@ -1,5 +1,17 @@
 package in.ashokit.binding;
 
+import lombok.Data;
+
+@Data
 public class IncomeDetails {
+	
+	private Integer caseNumber;
+	
+	private Long monthlySalIncome;
+	
+	private Long propertyIncome;
+	
+	private Long rentIncome;
+	
 
 }
