e3bc34ab03fb901c4b656cdcf6a12afc0d448e63
KamalTade <110621652+KamalTade@users.noreply.github.com>
2022-10-15 13:07:20 +0530

    Update KidsDetails.java

--- src/main/java/in/ashokit/binding/KidsDetails.java
+++ src/main/java/in/ashokit/binding/KidsDetails.java
@@ -1,5 +1,15 @@
 package in.ashokit.binding;
 
+import lombok.Data;
+
+@Data
 public class KidsDetails {
+	
+	private String kidname;
+	
+	private Integer kidAge;
+	
+	private Integer kidSSN;
+	
 
 }
